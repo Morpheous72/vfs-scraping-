c329c6b38e8fe3f965695b108f58bf5c6e48df2d
MorpheouS72 <nazimbeggour@gmail.com>
2023-02-08 00:20:26 +0100

    Update countries.js

--- models/countries.js
+++ models/countries.js
@@ -1,26 +1,32 @@
 const GERMANY = { 
-  VFS_URL : "https://visa.vfsglobal.com/ind/en/deu/login",
+  VFS_URL : "https://visa.vfsglobal.com/dza/fr/deu/login",
   VISA_COUNTRY:"Germany"
 }
 
 const FRANCE = {
-  VFS_URL : "https://visa.vfsglobal.com/ind/en/fra/login",
+  VFS_URL : "https://visa.vfsglobal.com/dza/fr/fra/login",
   VISA_COUNTRY :"France"
 }
 
 const ITALY = {
-  VFS_URL : "https://visa.vfsglobal.com/ind/en/ita/login",
+  VFS_URL : "https://visa.vfsglobal.com/dza/fr/ita/login",
   VISA_COUNTRY :"Italy"
 }
 
 const SWISS = {
-  VFS_URL : "https://visa.vfsglobal.com/ind/en/che/login",
+  VFS_URL : "https://visa.vfsglobal.com/dza/fr/che/login",
   VISA_COUNTRY :"Swiss"
+}
+
+const SWINETHERLANDSSS = {
+  VFS_URL : "https://visa.vfsglobal.com/dza/fr/nld/login",
+  VISA_COUNTRY :"Netherlands"
 }
 
 module.exports = {
   GERMANY,
   FRANCE,
   ITALY,
-  SWISS
+  SWISS,
+  NETHERLANDS
 }
